4f2f8d771c983f22275f5631e86e8291d3c17c4d
Llonvne <work@llonvne.cn>
2022-05-01 14:07:16 +0800

    修复 disChoice 错误
    优化了 BG 和 scoreboard部分错误

--- src/GameItem/ScoreBoard.py
+++ src/GameItem/ScoreBoard.py
@@ -8,6 +8,11 @@ class Scoreboard(GameItem):
         pass
 
     def __init__(self):
+        """
+        Initialize the scoreboard
+        """
+
+        # 数字区域
         self.one = 0
         self.two = 0
         self.three = 0
@@ -15,20 +20,32 @@ class Scoreboard(GameItem):
         self.five = 0
         self.six = 0
 
-        self.numCount = 0
+        # 奖励分数
         self.reward = 0
 
+        # 组合分数
         self.total = 0
-        self.foutSame = 0
+        self.fourSame = 0
         self.calabash = 0
         self.smallStraights = 0
         self.largeStraights = 0
         self.speedboat = 0
 
-    def totalScore(self):
+    def totalScore(self) -> int:
+        """
+        计算上述总分
+        :return: 总分
+        """
         return self.one + self.two + self.three + self.four + self.five + self.six + \
-               self.reward + self.total + self.foutSame + self.calabash + \
+               self.reward + self.total + self.fourSame + self.calabash + \
                self.smallStraights + self.largeStraights + self.speedboat
 
-    def totalScoreofNumber(self):
+    def totalScoreofNumber(self) -> int:
+        """
+        计算数字区域总分
+        :return: int
+        """
         return self.one + self.two + self.three + self.four + self.five + self.six
+
+    def isReward(self) -> bool:
+        return self.totalScoreofNumber() >= 63
--- src/tools/disBG.py
+++ src/tools/disBG.py
@@ -4,5 +4,10 @@ import pygame
 
 
 def disBg(screen: pygame.Surface) -> None:
+    """
+    在屏幕上显示默认背景，常用于刷新屏幕
+    :param screen:
+    :return: None
+    """
     board = pygame.image.load(os.path.join('Media', 'Pic', 'board2.png'))
     screen.blit(board, (0, 0))

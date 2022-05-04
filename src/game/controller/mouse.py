c339129f2c66748563a7e1f0366c9bdc65ea438d
Llonvne <work@llonvne.cn>
2022-05-04 21:56:00 +0800

    Update mouse.py

--- src/game/controller/mouse.py
+++ src/game/controller/mouse.py
@@ -6,13 +6,14 @@ def event(event, postFunc) -> None:
     print(event.__dict__.get('pos')[0], event.__dict__.get('pos')[1])
     if 720 < event.__dict__.get('pos')[0] < 797 and 597 < event.__dict__.get('pos')[1] < 678:
         postFunc("骰子事件", "投")
-    elif 551 < event.__dict__.get('pos')[0] < 651 and 64 < event.__dict__.get('pos')[1] < 157:
+    elif 551 < event.__dict__.get('pos')[0] < 651 and 200 < event.__dict__.get('pos')[1] < 300:
         post(Event(pygame.USEREVENT, {'类型': '骰子事件', '描述': '选择', 'no': 0}))
     elif 651 < event.__dict__.get('pos')[0] < 751 and 64 < event.__dict__.get('pos')[1] < 157:
+    elif 651 < event.__dict__.get('pos')[0] < 751 and 200 < event.__dict__.get('pos')[1] < 300:
         post(Event(pygame.USEREVENT, {'类型': '骰子事件', '描述': '选择', 'no': 1}))
-    elif 751 < event.__dict__.get('pos')[0] < 851 and 64 < event.__dict__.get('pos')[1] < 157:
+    elif 751 < event.__dict__.get('pos')[0] < 851 and 200 < event.__dict__.get('pos')[1] < 300:
         post(Event(pygame.USEREVENT, {'类型': '骰子事件', '描述': '选择', 'no': 2}))
-    elif 851 < event.__dict__.get('pos')[0] < 981 and 64 < event.__dict__.get('pos')[1] < 157:
+    elif 851 < event.__dict__.get('pos')[0] < 981 and 200 < event.__dict__.get('pos')[1] < 300:
         post(Event(pygame.USEREVENT, {'类型': '骰子事件', '描述': '选择', 'no': 3}))
-    elif 951 < event.__dict__.get('pos')[0] < 1051 and 64 < event.__dict__.get('pos')[1] < 157:
+    elif 951 < event.__dict__.get('pos')[0] < 1051 and 200 < event.__dict__.get('pos')[1] < 300:
         post(Event(pygame.USEREVENT, {'类型': '骰子事件', '描述': '选择', 'no': 4}))

8d1ea524ad1ae6990572487945c90af364a38a69
Llonvne <work@llonvne.cn>
2022-05-31 08:42:12 +0800

    MIT License
    将 SpeedBoatDice -> Yahtzee

--- src/Main.py
+++ src/Main.py
@@ -4,9 +4,9 @@ Authors: Llonvne, Daiwu
 @link https://github.com/Llonvne/SpeedboatDice/
 """
 # 导入库文件
-from src import SpeedboatDice
+from src import Yahtzee
 
 # 初始化游戏进程,调用 run 函数
-process = SpeedboatDice.SpeedBoatDice()
+process = Yahtzee.SpeedBoatDice()
 process.run()
 
